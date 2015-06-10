f41647f3321baeb331981cf12dba26f0a8485e6d
john liang <fantasticfour@126.com>
2015-06-11 00:36:42 +0800

    add remote build file

--- build.sh
+++ build.sh
@@ -7,12 +7,8 @@ javac src/com/john/DownloadServlet.java -cp ./lib/javaee.jar -d classes
 javac src/com/john/PlayServlet.java -cp ./lib/javaee.jar -d classes
 
 echo "cp classes"
-cp ./classes/com/john/DownloadServlet.class ~/tools/tomcat/webapps/servlet/WEB-INF/classes/com/john/
-cp ./classes/com/john/PlayServlet.class ~/tools/tomcat/webapps/servlet/WEB-INF/classes/com/john/
+cp ./classes/com/john/DownloadServlet.class ~/tomcat/webapps/hkcaijing/WEB-INF/classes/com/john/
+cp ./classes/com/john/PlayServlet.class ~/tomcat/webapps/hkcaijing/WEB-INF/classes/com/john/
 
 echo "cp web.xml"
 cp ./WEB-INF/web.xml ~/tools/tomcat/webapps/servlet/WEB-INF/
-
-echo "restart tomcat"
-/Users/john/tools/tomcat/bin/catalina.sh stop
-/Users/john/tools/tomcat/bin/catalina.sh start
---
+++ build_local.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
+
+echo "javac classes"
+javac src/com/john/DownloadServlet.java -cp ./lib/javaee.jar -d classes
+javac src/com/john/PlayServlet.java -cp ./lib/javaee.jar -d classes
+
+echo "cp classes"
+cp ./classes/com/john/DownloadServlet.class ~/tools/tomcat/webapps/servlet/WEB-INF/classes/com/john/
+cp ./classes/com/john/PlayServlet.class ~/tools/tomcat/webapps/servlet/WEB-INF/classes/com/john/
+
+echo "cp web.xml"
+cp ./WEB-INF/web.xml ~/tools/tomcat/webapps/servlet/WEB-INF/
+
+echo "restart tomcat"
+/Users/john/tools/tomcat/bin/catalina.sh stop
+/Users/john/tools/tomcat/bin/catalina.sh start
