011499e7b7d1601619ee4b97489b90464ef5c451
john liang <fantasticfour@126.com>
2015-06-11 00:46:01 +0800

    update remote build file

--- build.sh
+++ build.sh
@@ -6,9 +6,17 @@ echo "javac classes"
 javac src/com/john/DownloadServlet.java -cp ./lib/javaee.jar -d classes
 javac src/com/john/PlayServlet.java -cp ./lib/javaee.jar -d classes
 
+echo "create hkcaijing webapp"
+mkdir ~/tomcat/webapps/hkcaijing
+mkdir ~/tomcat/webapps/hkcaijing/WEB-INF/
+mkdir ~/tomcat/webapps/hkcaijing/WEB-INF/classes
+mkdir ~/tomcat/webapps/hkcaijing/WEB-INF/classes/com
+mkdir ~/tomcat/webapps/hkcaijing/WEB-INF/classes/com/john
+
+
 echo "cp classes"
 cp ./classes/com/john/DownloadServlet.class ~/tomcat/webapps/hkcaijing/WEB-INF/classes/com/john/
 cp ./classes/com/john/PlayServlet.class ~/tomcat/webapps/hkcaijing/WEB-INF/classes/com/john/
 
 echo "cp web.xml"
-cp ./WEB-INF/web.xml ~/tomcat/webapps/servlet/WEB-INF/
+cp ./WEB-INF/web.xml ~/tomcat/webapps/hkcaijing/WEB-INF/
