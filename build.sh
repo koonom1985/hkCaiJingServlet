572a643bb8d58d9c277ea09e6aaa2ead7f3aa5d7
john liang <fantasticfour@126.com>
2015-06-11 00:49:38 +0800

    update remote build file

--- build.sh
+++ build.sh
@@ -3,20 +3,21 @@
 export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 
 echo "javac classes"
+mkdir classes
 javac src/com/john/DownloadServlet.java -cp ./lib/javaee.jar -d classes
 javac src/com/john/PlayServlet.java -cp ./lib/javaee.jar -d classes
 
 echo "create hkcaijing webapp"
-mkdir ~/tomcat/webapps/hkcaijing
-mkdir ~/tomcat/webapps/hkcaijing/WEB-INF/
-mkdir ~/tomcat/webapps/hkcaijing/WEB-INF/classes
-mkdir ~/tomcat/webapps/hkcaijing/WEB-INF/classes/com
-mkdir ~/tomcat/webapps/hkcaijing/WEB-INF/classes/com/john
+mkdir ~/tomcat7/webapps/hkcaijing
+mkdir ~/tomcat7/webapps/hkcaijing/WEB-INF/
+mkdir ~/tomcat7/webapps/hkcaijing/WEB-INF/classes
+mkdir ~/tomcat7/webapps/hkcaijing/WEB-INF/classes/com
+mkdir ~/tomcat7/webapps/hkcaijing/WEB-INF/classes/com/john
 
 
 echo "cp classes"
-cp ./classes/com/john/DownloadServlet.class ~/tomcat/webapps/hkcaijing/WEB-INF/classes/com/john/
-cp ./classes/com/john/PlayServlet.class ~/tomcat/webapps/hkcaijing/WEB-INF/classes/com/john/
+cp ./classes/com/john/DownloadServlet.class ~/tomcat7/webapps/hkcaijing/WEB-INF/classes/com/john/
+cp ./classes/com/john/PlayServlet.class ~/tomcat7/webapps/hkcaijing/WEB-INF/classes/com/john/
 
 echo "cp web.xml"
-cp ./WEB-INF/web.xml ~/tomcat/webapps/hkcaijing/WEB-INF/
+cp ./WEB-INF/web.xml ~/tomcat7/webapps/hkcaijing/WEB-INF/
